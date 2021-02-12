aed5245581b559e0af0b254f72d2bcaca85b1db8
khs6226 <khsun6226@naver.com>
2021-02-11 23:39:13 -0800

    render result page according to the search result.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
  (Make sure you also specify on the Google Doc)
 */
 const express = require("express");
+const fs = require('fs');
 
 let app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -33,6 +34,23 @@ app.get("/myListQueryString", (req, res) => {
 
 app.get("/search/:movieName", (req, res) => {
   // Add your implementation here
+  let movieTitle = req.params.movieName.slice(1);
+  console.log('specific', movieTitle);
+  fs.readFile('movieDescriptions.txt', 'utf-8', (err, data) => {
+    let movieObj = {};
+
+    if (err) throw err;
+    if (data.includes(movieTitle)) {
+      let txt = data.split('\n');
+      txt.forEach(elem => {
+        let splitElem = elem.split(':');
+        console.log('split', splitElem);
+        movieObj[splitElem[0]] = splitElem[1];
+      })
+    }
+    console.log('movieObj', movieObj);
+    res.render('pages/searchResult', {movieName : movieTitle, movieDesc : movieObj[movieTitle]});
+  });
 });
 
 app.listen(3000, () => {
